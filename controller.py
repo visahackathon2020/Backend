d1835621ff9a41c8af9a3df166b7069b5fabe136
kpence <pencekyle@gmail.com>
2020-06-19 17:11:56 -0500

    Added requirements, moved certs to new folder

--- controller.py
+++ controller.py
@@ -1,11 +1,12 @@
 #!flask/bin/python
-from flask import Flask
+from flask import Flask, jsonify
 import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() # Load environment variables
 app = Flask(__name__)
+certs_dir_path = os.path.dirname(os.path.realpath(__file__)) + '/certs'
 
 @app.route('/hello-world')
 def hello_world():
@@ -14,16 +15,15 @@ def hello_world():
 @app.route('/visa-api-test')
 def visa_api_test():
     url = 'https://sandbox.api.visa.com/vdp/helloworld'
-    dir_path = os.path.dirname(os.path.realpath(__file__))
     user_id = os.getenv("userid")
     password = os.getenv("password")
     headers = ""
     body = ""
 
-    #verify = (dir_path + '/DigiCertGlobalRootCA.crt'),
+    #verify = (certs_dir_path + '/DigiCertGlobalRootCA.crt'),
     r = requests.get(url,
         verify = (""),
-        cert = (dir_path + '/cert.pem',dir_path + '/key_54a11bcc-fab0-449d-9092-4fa83d6a557b.pem'),
+        cert = (certs_dir_path + '/cert.pem',certs_dir_path + '/key_54a11bcc-fab0-449d-9092-4fa83d6a557b.pem'),
         headers = headers,
         auth = (user_id, password),
         data = body)
@@ -31,4 +31,5 @@ def visa_api_test():
     return r.content
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True,ssl_context=(certs_dir_path + '/server.crt',certs_dir_path + '/server.key'))
+    app.run(debug=True,ssl_context='adhoc')
