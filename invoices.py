37ab6725eae676379926668200571f1e0bcd7134
kpence <pencekyle@gmail.com>
2020-06-23 22:21:34 +0000

    Now invoices get scheduled for deletion

--- invoices.py
+++ invoices.py
@@ -8,6 +8,7 @@ from db import database
 from helpers import decorate_all_methods, return_status
 import random, string
 import json
+import threading, sched, time
 
 # Helper functions
 userCodes = {}
@@ -21,6 +22,12 @@ def businessToUserCode(businessCode):
 
 def userToBusinessCode(userCode):
     return userCodes[userCode]
+
+scheduler = sched.scheduler(time.time, time.sleep)
+def scheduleExpirationHandler(apiRef, userCode, timeUntilExpiration=60*60):
+    print(timeUntilExpiration)
+    scheduler.enter(timeUntilExpiration, 1, apiRef.delete, kwargs={'id':userCode})
+    scheduler.run()
 
 # Invoice RESTful resource Controller
 @decorate_all_methods(return_status)
@@ -36,9 +43,14 @@ class Invoice(Resource):
     def post(self, id=None):
         new_invoice_ref = database.collection(u'invoices').document()
         new_invoice_ref.set(json.loads(request.data))
-        return {'invoiceCode': businessToUserCode(new_invoice_ref.id)}
+        code = businessToUserCode(new_invoice_ref.id)
+        # Schedule the expiration
+        t = threading.Thread(target=scheduleExpirationHandler, args=(self,code,))
+        t.start()
+        return {'invoiceCode': code}
 
     def delete(self, id=None):
+        print("Invoice Delete Called")
         code = userToBusinessCode(id)
         doc_ref = database.collection(u'invoices').document(code)
         doc_ref.delete()
