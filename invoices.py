d31fa92b3a88ce5240f2bec5acf30f72ff0c2a54
anqingchen <benjamin.c0427@gmail.com>
2020-06-28 17:02:23 -0500

    delete docs on code-generation

--- invoices.py
+++ invoices.py
@@ -13,6 +13,7 @@ import threading, sched, time
 from generator import TokenGenerator
 from marshmallow import ValidationError
 from firebase_admin import auth
+from firebase_admin import firestore
 
 # Helper functions
 codeGenerator = TokenGenerator()
@@ -21,8 +22,18 @@ def generateDocRef():
     while True:
         code = codeGenerator.generate()
         new_invoice_ref = database.collection(u'invoices').document(code)
-        if not new_invoice_ref.get().exists:
+        new_invoice = new_invoice_ref.get()
+        if not new_invoice.exists:
             return new_invoice_ref
+        else:
+            invoice_dict = new_invoice.to_dict()
+            created = invoice_dict['created']
+            difference = time.time() - created
+            if difference > 86400:
+                # delete the doc
+                new_invoice_ref.delete()
+                # create a new one again
+                return database.collection(u'invoices').document(code)
 
 scheduler = sched.scheduler(time.time, time.sleep)
 def scheduleExpirationHandler(apiRef, userCode, timeUntilExpiration=60*60):
@@ -59,6 +70,7 @@ class Invoice(Resource):
             decoded_token = auth.verify_id_token(result['merchantToken'])
             del result['merchantToken']
             result['merchantId'] = decoded_token['uid']
+        result['created'] = time.time()
         new_invoice_ref.set(result)
         code = new_invoice_ref.id
         # Schedule the expiration
